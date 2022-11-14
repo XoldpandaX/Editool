17acc630a0d37e99c45ef9f7287a384fe9db7422
XoldpandaX <rembo-87@yandex.ru>
2022-11-14 21:04:09 +0100

    feat: install antd with dark-compact theme

---
+++ craco.config.js
@@ -0,0 +1,21 @@
+const CracoLess = require('craco-less');
+const { getThemeVariables } = require('antd/dist/theme');
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoLess,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: getThemeVariables({
+              dark: true, // Enable dark mode,
+              compact: true, // Enable compact mode
+            }),
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button } from 'antd';
+
+import './App.less';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img
-          src={logo}
-          className="App-logo"
-          alt="logo"
-        />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button
+        type="primary"
+        onClick={() => console.error('hello world')}
+      >
+        antd button
+      </Button>
     </div>
   );
 }
