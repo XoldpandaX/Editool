eb6ed77afc0e2219b8447b57511d3bc957da127b
XoldpandaX <rembo-87@yandex.ru>
2022-11-18 23:47:13 +0100

    feat: add PageEditDocument

--- src/pages/edit-document/page-edit-document.tsx
+++ src/pages/edit-document/page-edit-document.tsx
@@ -1,10 +1,19 @@
 import { FC } from 'react';
-import { Editor } from '@/modules/editor/editor';
+import { Editor } from '@/editor';
+import { Typography, Divider } from 'antd';
 
 import './page-edit-document.scss';
 
+const { Title, Paragraph, Text } = Typography;
+
 export const PageEditDocument: FC = () => (
   <div className="page-edit-document">
+    <Paragraph>Paragraph</Paragraph>
+    <Text>Text</Text>
+    <Title level={1}>Hello</Title>
+    <Title level={2}>Hello</Title>
+    <Title level={3}>Hello</Title>
+    <Divider />
     <Editor />
   </div>
 );
