6cc11f89c2fda87574785241eac6ba74c500e8ce
XoldpandaX <denis.d.shakin@gmail.com>
2022-11-20 15:17:34 +0100

    refactor(EditorActionMenu): receive CommandManager from the props

--- src/editor/components/editor-action-menu/editor-action-menu.tsx
+++ src/editor/components/editor-action-menu/editor-action-menu.tsx
@@ -1,44 +1,41 @@
 import { FC } from 'react';
 import { BubbleMenu, Editor } from '@tiptap/react';
-import { CommandManager } from '../../services/command-manager';
+import { ICommandManager } from '@/editor/types';
 
 import './editor-action-menu.scss';
 
 type EditorActionMenuProps = {
   editor: Editor;
+  commandManager: ICommandManager;
 };
 
-const EditorActionMenu: FC<EditorActionMenuProps> = ({ editor }) => {
-  const commandManager = new CommandManager(editor);
-
-  return (
-    <BubbleMenu
-      className="editor-action-menu"
-      editor={editor}
-      tippyOptions={{ duration: 100 }}
-      updateDelay={1000}
-      shouldShow={() => true}
+const EditorActionMenu: FC<EditorActionMenuProps> = ({ editor, commandManager }) => (
+  <BubbleMenu
+    className="editor-action-menu"
+    editor={editor}
+    tippyOptions={{ duration: 100 }}
+    updateDelay={1000}
+    shouldShow={() => true}
+  >
+    <button
+      onClick={() => commandManager.exec('toggleBold')}
+      className={editor.isActive('bold') ? 'is-active' : ''}
     >
-      <button
-        onClick={() => commandManager.exec('toggleBold')}
-        className={editor.isActive('bold') ? 'is-active' : ''}
-      >
-        bold
-      </button>
-      <button
-        onClick={() => commandManager.exec('toggleItalic')}
-        className={editor.isActive('italic') ? 'is-active' : ''}
-      >
-        italic
-      </button>
-      <button
-        onClick={() => commandManager.exec('toggleStrike')}
-        className={editor.isActive('strike') ? 'is-active' : ''}
-      >
-        strike
-      </button>
-    </BubbleMenu>
-  );
-};
+      bold
+    </button>
+    <button
+      onClick={() => commandManager.exec('toggleItalic')}
+      className={editor.isActive('italic') ? 'is-active' : ''}
+    >
+      italic
+    </button>
+    <button
+      onClick={() => commandManager.exec('toggleStrike')}
+      className={editor.isActive('strike') ? 'is-active' : ''}
+    >
+      strike
+    </button>
+  </BubbleMenu>
+);
 
 export default EditorActionMenu;
