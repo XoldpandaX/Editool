34870a6f4211c40b1b394b50ea862d50dd670d92
XoldpandaX <denis.d.shakin@gmail.com>
2022-11-20 15:16:51 +0100

    refactor(CommandManager): split commands by editing and state

--- src/editor/services/command-manager.ts
+++ src/editor/services/command-manager.ts
@@ -3,11 +3,15 @@ import { isNil } from 'remeda';
 import { Command, ICommandManager } from '../types';
 
 export class CommandManager implements ICommandManager {
+  private static readonly stateCommands = ['blur'];
+
   constructor(protected readonly editor: Editor | null) {}
 
   public exec = (command: Command): void => {
     if (isNil(this.editor)) this.throwAnError(command);
-    if (!this.isActiveCommand(command)) this.editor.chain().focus()[command]().run();
+
+    if (CommandManager.stateCommands.includes(command)) this.editor.commands[command]();
+    else this.editor.chain().focus()[command]().run();
   };
 
   public isActiveCommand = (command: Command): boolean => (isNil(this.editor) ? false : this.editor.isActive(command));
--- src/editor/types.ts
+++ src/editor/types.ts
@@ -1,4 +1,6 @@
-export type Command = 'toggleBold' | 'toggleItalic' | 'toggleStrike' | 'toggleCode' | 'toggleUnderline';
+export type StateCommand = 'blur';
+export type EditingCommand = 'toggleBold' | 'toggleItalic' | 'toggleStrike' | 'toggleCode' | 'toggleUnderline';
+export type Command = EditingCommand | StateCommand;
 
 export interface ICommandManager {
   exec(command: Command): void;
