af57fad9b8732ba82c973550825a325b54962afd
XoldpandaX <denis.d.shakin@gmail.com>
2022-11-20 15:18:16 +0100

    feat(Editor): blur on ESC key press && provide CommandManager

--- src/editor/index.tsx
+++ src/editor/index.tsx
@@ -1,5 +1,6 @@
-import { createRef, RefObject } from 'react';
+import { createRef, RefObject, KeyboardEvent } from 'react';
 import { useEditor, EditorContent } from '@tiptap/react';
+import { CommandManager } from './services/command-manager';
 import editorConfig from './editor-config';
 import EditorActionMenu from './components/editor-action-menu/editor-action-menu';
 
@@ -19,16 +20,28 @@ export const Editor = () => {
       console.info(ed.view.state.selection.empty);
     },
   });
+  const commandManager = new CommandManager(editor);
+
+  const onKeyDown = (e: KeyboardEvent<HTMLDivElement>): void => {
+    if (!editor) return;
+    if (e.key === 'Escape') commandManager.exec('blur');
+  };
 
   return (
     <div
       className="notely-editor"
       ref={refEditor}
     >
-      {editor ? <EditorActionMenu editor={editor} /> : null}
+      {editor ? (
+        <EditorActionMenu
+          editor={editor}
+          commandManager={commandManager}
+        />
+      ) : null}
       <EditorContent
         className="notely-editor__content"
         editor={editor}
+        onKeyDown={onKeyDown}
       />
     </div>
   );
